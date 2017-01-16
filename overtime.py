2bcba84e6449ee530ccfe8fd58fb63122c7e5df8
Sarah <Sarahkgg@users.noreply.github.com>
2017-01-16 15:35:04 -0800

    Add files via upload

---
+++ overtime.py
@@ -0,0 +1,11 @@
+hours = float(input("How many hours were worked? "))
+rate = float(input("At what rate of pay? "))
+
+standardpay = (hours)*(rate)
+
+overtimepay = (40 * rate) + ((hours - 40) * (rate * 1.5))
+
+if hours >= 40: 
+	print (overtimepay)
+else:
+	print (standardpay)
